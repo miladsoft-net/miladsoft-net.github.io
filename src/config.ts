763a4441dc91bceadf2bcd0017e51315da2f166a
Milad Raeisi <miladsoft@yahoo.com>
2025-01-02 00:23:50 +0400

    Lorem Ipsum

--- src/config.ts
+++ src/config.ts
@@ -57,8 +57,8 @@ export const navBarConfig: NavBarConfig = {
 
 export const profileConfig: ProfileConfig = {
   avatar: 'assets/images/demo-avatar.png',  // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-  name: 'Lorem Ipsum',
-  bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  name: 'Miladsoft',
+  bio: 'Miladsoft dolor sit amet, consectetur adipiscing elit.',
   links: [
     {
       name: 'Twitter',
